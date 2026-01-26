27aead8a7f9c5ca67865e686e0b52f27af567bb6
Jash Parekh <jparekh1@wayfair.com>
2026-01-26 12:18:08 +0530

    Release version 0.1.0
    - Add static version 0.1.0 in pyproject.toml and __init__.py
    - Remove setuptools-scm dependency (using static versioning)
    - Add CHANGELOG.md documenting all changes since 0.0.3

--- pyterraformer/__init__.py
+++ pyterraformer/__init__.py
@@ -1,16 +1,10 @@
-from importlib.metadata import PackageNotFoundError, version
-
 from .config import Config
 from .core.workspace import TerraformWorkspace
 from .serializer import HumanSerializer
 from .terraform.backends import LocalBackend
 from .terraform.terraform import Terraform
 
-try:
-    __version__ = version("pyterraformer")
-except PackageNotFoundError:
-    # package is not installed
-    pass
+__version__ = "0.1.0"
 
 __all__ = [
     "HumanSerializer",
